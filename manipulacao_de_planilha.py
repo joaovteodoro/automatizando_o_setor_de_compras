253a336444f74189d16b59422f5322163ba2aee3
João Vítor Teodoro Santos <joaovitorts@hotmail.com>
2025-05-23 13:11:13 -0300

    Reorganizando o programa utilizando funções.

--- manipulacao_de_planilha.py
+++ manipulacao_de_planilha.py
@@ -1,86 +1,108 @@
 import pandas as pd
 
+# ABRINDO E MINERANDO AS PLANILHAS DE SOLICITAÇÕES (SDE) DO SISTEMA
+def abrindo_planilhas_do_manager(caminho_planilha, cor_sdes):
+    planilha_sde = pd.read_excel(caminho_planilha)
+    lista_de_sdes_sistema = planilha_sde["Sol. Web"].tolist()  # SDE VEM COM 'Compra-' na frente
+    lista_de_sdes_sistema = [item.split("-")[1] for item in lista_de_sdes_sistema]  # Retira o 'Compras-'
+    print(f"SDE(s) {cor_sdes}: {len(lista_de_sdes_sistema)}")
+    print(lista_de_sdes_sistema)
+    print()
+    return(lista_de_sdes_sistema)
+
+# ABRINDO E MINERANDO A PLANILHA PRINCIPAL
+def abrindo_planilha_principal(caminho_planilha_principal):
+
+    planilha_principal = pd.read_excel(caminho_planilha_principal, sheet_name=1, header=2)
+    return planilha_principal
+
+# SEPARANDO A LISTA DE SOLICITAÇÕES VERMELHA(S) ('PENDENTE' E 'A FAZER') E AMARELA(S) (PARCIAL) DA PLANILHA PRINCIPAL
+def salvando_sdes_da_planilha_principal(cor_sdes):
+
+    if cor_sdes == "VERMELHAS":
+        contagem_pendente = (planilha_principal["STATUS"] == "PENDENTE").sum() + (planilha_principal["STATUS"] == "A FAZER").sum()
+        print(f"SDE(s) VERMELHA(S): {contagem_pendente}")
+        filtro_sdes = planilha_principal[(planilha_principal["STATUS"] == "PENDENTE") | (planilha_principal["STATUS"] == "A FAZER")]
+    elif cor_sdes == "AMARELAS":
+        contagem_parcial = (planilha_principal["STATUS"] == "PARCIAL").sum()
+        print(f"SDE(s) AMARELA(S): {contagem_parcial}")
+        filtro_sdes = planilha_principal[planilha_principal["STATUS"] == "PARCIAL"]
+
+    lista_de_sdes_planilha_principal = filtro_sdes["SDE WEB"].tolist() #TRANSFORMA A TUPLA EM LISTA
+    lista_de_sdes_planilha_principal = [str(elemento) for elemento in lista_de_sdes_planilha_principal]  # TRANSFORMA TODOS OS ELEMENTOS EM STR
+    print(lista_de_sdes_planilha_principal)
+    print()
+    return(lista_de_sdes_planilha_principal)
+
+# COMPARANDO SDES DO SISTEMA COM AS SDES DA PLANILHA 
+def sde_so_esta_no_manager(lista_manager, lista_principal):
+    sde_so_no_manager = lista_manager - lista_principal
+    return sde_so_no_manager
+
+# INDICANDO AS SOLICITAÇÕES LANÇADAS ERRADAS (VERMELHA QUE ESTÁ LANÇADA COMO AMARELA, E VICE E VERSA)
+def sdes_de_cores_divergentes(sde_lancada_apenas_no_manager, lista_principal):
+    sde_de_cor_divergente_na_principal = set()
+
+    for sde in sde_lancada_apenas_no_manager: #confere se a sde está realmente apenas no manager, ou se foi lançada na principal com o status errado
+        if sde in lista_principal:
+            sde_de_cor_divergente_na_principal.add(sde) 
+    
+    sde_lancada_apenas_no_manager = sde_lancada_apenas_no_manager - sde_de_cor_divergente_na_principal
+
+    return (sde_de_cor_divergente_na_principal, sde_lancada_apenas_no_manager)
+
+# INDICA AS SOLICITAÇÕES LANÇADAS ERRADAS (DEVE(M) SER LANÇADA(S) COMO VERDE(S) ('CONCLUIDA')) 
+def sde_que_devem_ser_concluidas():
+    total_em_aberto_planilha = set(lista_de_sdes_vermelhas_principal + lista_de_sdes_amarelas_principal)
+    total_em_aberto_sistema = set(lista_de_sdes_vermelhas_sistema + lista_de_sdes_amarelas_sistema)
+    devem_ser_concluidas = total_em_aberto_planilha - total_em_aberto_sistema
+    return devem_ser_concluidas
+
+
+############################################################################################################
+
+
 print()
 print("SISTEMA:")
 print("----------------")
 
-# SALVANDO A LISTA DE SDES VERMELHA(S) NO SISTEMA
-caminho_sde_vermelhas = r"C:\\Users\\joaov\\Desktop\\PROGRAMA\\planilha_sdes_vermelhas.xlsx"
-planilha_sde_vermelhas = pd.read_excel(caminho_sde_vermelhas)
-lista_de_sdes_vermelhas_sistema = planilha_sde_vermelhas["Sol. Web"].tolist()  # SDE VEM COM 'Compra-' na frente
-lista_de_sdes_vermelhas_sistema = [item.split("-")[1] for item in lista_de_sdes_vermelhas_sistema]  # Retira o 'Compras-'
-print(f"SDE(s) VERMELHA(S): {len(lista_de_sdes_vermelhas_sistema)}")
-print(lista_de_sdes_vermelhas_sistema)
-
-# SALVANDO A LISTA DE SDES AMARELA(S) NO SISTEMA
+# ABRINDO E MINERANDO AS PLANILHAS DE SOLICITAÇÕES (SDE) DO SISTEMA
+caminho_sdes_vermelhas = r"C:\\Users\\joaov\\Desktop\\PROGRAMA\\planilha_sdes_vermelhas.xlsx"
 caminho_sdes_amarelas = r"C:\\Users\\joaov\\Desktop\\PROGRAMA\\planilha_sdes_amarelas.xlsx"
-planilha_sdes_amarelas = pd.read_excel(caminho_sdes_amarelas)
-lista_de_sdes_amarelas_sistema = planilha_sdes_amarelas["Sol. Web"].tolist()  # SDE VEM COM 'Compra-' na frente
-lista_de_sdes_amarelas_sistema = [item.split("-")[1] for item in lista_de_sdes_amarelas_sistema]  # Retira o 'Compras-'
-print(f"SDE(s) AMARELA(S): {len(lista_de_sdes_amarelas_sistema)}")
-print(lista_de_sdes_amarelas_sistema)
+lista_de_sdes_vermelhas_sistema = abrindo_planilhas_do_manager(caminho_sdes_vermelhas, "VERMELHA(S)")
+lista_de_sdes_amarelas_sistema = abrindo_planilhas_do_manager(caminho_sdes_amarelas, "AMARELA(S)" )
 
 print()
 print("PLANILHA:")
 print("----------------")
-#############################################
 
-# ABRINDO PLANILHA PRINCIPAL
-caminho_principal = r"C:\\Users\\joaov\\Desktop\\PROGRAMA\\planilha_principal.xlsm"
-planilha_principal = pd.read_excel(caminho_principal, sheet_name=1, header=2)
-# sheet_name = 1 - Seleciona a segunda aba da planilha
-# header mostra em qual linha começa o cabeçalho
+# ABRINDO E MINERANDO PLANILHA PRINCIPAL
+caminho_planilha_principal = r"C:\\Users\\joaov\\Desktop\\PROGRAMA\\planilha_principal.xlsm"
+planilha_principal = abrindo_planilha_principal(caminho_planilha_principal)
 
-# SALVANDO A LISTA DE SDES VERMELHA(S) NA PLANILHA ('PENDENTE' E 'A FAZER')
-contagem_pendente = (planilha_principal["STATUS"] == "PENDENTE").sum() + (planilha_principal["STATUS"] == "A FAZER").sum()
-print(f"SDE(s) VERMELHA(S): {contagem_pendente}")
-filtro_pendentes = planilha_principal[planilha_principal["STATUS"] == "PENDENTE"]
-filtro_a_fazer = planilha_principal[planilha_principal["STATUS"] == "A FAZER"]
-lista_de_sdes_vermelhas_principal = filtro_pendentes["SDE WEB"].tolist() + filtro_a_fazer["SDE WEB"].tolist()
-lista_de_sdes_vermelhas_principal = [
-    str(elemento) for elemento in lista_de_sdes_vermelhas_principal
-]  # TRANSFORMA TODOS OS ELEMENTOS EM STR
-print(lista_de_sdes_vermelhas_principal)
-
-# SALVANDO A LISTA DE SDES AMARELA(S) NA PLANILHA ('PARCIAL')
-contagem_parcial = (planilha_principal["STATUS"] == "PARCIAL").sum()
-print(f"SDE(s) AMARELA(S): {contagem_parcial}")
-filtro_parcial = planilha_principal[planilha_principal["STATUS"] == "PARCIAL"]
-lista_de_sdes_amarelas_principal = filtro_parcial["SDE WEB"].tolist()
-lista_de_sdes_amarelas_principal = [str(elemento) for elemento in lista_de_sdes_amarelas_principal]
-print(lista_de_sdes_amarelas_principal)
-print()
+# SEPARANDO A LISTA DE SOLICITAÇÕES VERMELHA(S) ('PENDENTE' E 'A FAZER') E AMARELA(S) (PARCIAL) DA PLANILHA PRINCIPAL
+lista_de_sdes_vermelhas_principal = salvando_sdes_da_planilha_principal("VERMELHAS")    
+lista_de_sdes_amarelas_principal = salvando_sdes_da_planilha_principal("AMARELAS") 
 
 # COMPARANDO SDES DO SISTEMA COM AS SDES DA PLANILHA
-vermelha_so_no_sistema = set(lista_de_sdes_vermelhas_sistema) - set(lista_de_sdes_vermelhas_principal)
-amarela_so_no_sistema = set(lista_de_sdes_amarelas_sistema) - set(lista_de_sdes_amarelas_principal)
+vermelha_so_no_manager = sde_so_esta_no_manager(set(lista_de_sdes_vermelhas_sistema), set(lista_de_sdes_vermelhas_principal))
+amarela_so_no_manager = sde_so_esta_no_manager(set(lista_de_sdes_amarelas_sistema), set(lista_de_sdes_amarelas_principal))
 
-# RESOLVENDO QUANDO UMA SDE VERMELHA E)STÁ LANÇADA COMO AMARELA, E VICE E VERSA
-amarela_que_deveria_ser_vermelha = set()
-for sde in vermelha_so_no_sistema:
-    if sde in lista_de_sdes_amarelas_principal:
-        amarela_que_deveria_ser_vermelha.add(
-            sde
-        )  # outra forma de trabalhar era separando a união entre a listaDeSdeAmarelasPrincipal e VermelhoSoNoSistema
-vermelha_so_no_sistema = vermelha_so_no_sistema - amarela_que_deveria_ser_vermelha
+# INDICANDO AS SOLICITAÇÕES LANÇADAS ERRADAS (VERMELHA QUE ESTÁ LANÇADA COMO AMARELA, E VICE E VERSA)
+amarela_que_deveria_ser_vermelha, vermelha_so_no_manager = sdes_de_cores_divergentes(vermelha_so_no_manager, lista_de_sdes_amarelas_principal)
+vermelha_que_deveria_ser_amarela, amarela_so_no_manager = sdes_de_cores_divergentes(amarela_so_no_manager, lista_de_sdes_vermelhas_principal)
 
-vermelha_que_deveria_ser_amarela = set()
-for sde in amarela_so_no_sistema:
-    if sde in lista_de_sdes_vermelhas_principal:
-        vermelha_que_deveria_ser_amarela.add(sde)
-amarela_so_no_sistema = amarela_so_no_sistema - vermelha_que_deveria_ser_amarela
+# INDICA AS SOLICITAÇÕES LANÇADAS ERRADA (DEVEM SER LANÇADA COMO VERDE (CONCLUIDA)) 
+devem_ser_concluidas = sde_que_devem_ser_concluidas()
 
-# RESOLVENDO QUANDO UMA SDE DEVERIA ESTAR CONCLUIDA NA PLANILHA, E NÃO ESTÁ
-total_em_aberto_planilha = set(lista_de_sdes_vermelhas_principal + lista_de_sdes_amarelas_principal)
-total_em_aberto_sistema = set(lista_de_sdes_vermelhas_sistema + lista_de_sdes_amarelas_sistema)
-devem_ser_concluidas = total_em_aberto_planilha - total_em_aberto_sistema
-
+# RESULTADO FINAL
+print()
 print("INFORMAÇÕES GERAIS:")
 print("----------------")
+
 print(f"A(s) SDE(s) {amarela_que_deveria_ser_vermelha} deve(m) ser alterada(s) para VERMELHO")
 print(f"A(s) SDE(s) {vermelha_que_deveria_ser_amarela} deve(m) ser alterada(s) para AMARELO")
-print(f"A(s) SDE(s) {devem_ser_concluidas} deve(m) ser alterada(s) para CONCLUIDO")
+print(f"A(s) SDE(s) {devem_ser_concluidas} deve(m) ser alterada(s) para VERDE")
 print()
-print(f"A(s) SDE(s) VERMELHA(S)) que deve(m) ser incluida(s) na planilha são: {vermelha_so_no_sistema}")
-print(f"A(s) SDE(s) AMARELA(S) que deve(m) ser incluida(s) na planilha são: {amarela_so_no_sistema}")
-print()
+print(f"A(s) SDE(s) VERMELHA(S)) que deve(m) ser incluida(s) na planilha são: {vermelha_so_no_manager}")
+print(f"A(s) SDE(s) AMARELA(S) que deve(m) ser incluida(s) na planilha são: {amarela_so_no_manager}")
--- salvando_planilha.py
+++ salvando_planilha.py
@@ -2,93 +2,82 @@ import os
 import time
 import pyautogui
 
-# FILTRANDO AS SDES DO USUÁRIO NO SISTEMA:
-pyautogui.moveTo(555, 400, 2)
-pyautogui.click()
-pyautogui.write("joaov")
-pyautogui.press("enter")
+# FILTRANDO O USUÁRIO NO SISTEMA
+def filtrar_usuario(usuario):
+    pyautogui.moveTo(555, 400, 2)
+    pyautogui.click()
+    pyautogui.write(usuario)
+    pyautogui.press("enter")
 
-# FILTRANDO AS SDES VERMELHAS
-pyautogui.moveTo(1500, 345, 2)
-pyautogui.click()
-time.sleep(2)
-pyautogui.moveTo(1500, 405, 2)
-pyautogui.press("enter")
-time.sleep(2)
+# FILTRANDO AS SDES
+def filtrar_sdes(coordenadas):
+    x, y, duracao = coordenadas
+    pyautogui.moveTo(1500, 345, 2)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(x, y, duracao)
+    pyautogui.press("enter")
+    time.sleep(2)
 
 # FILTRANDO A DATA DE PESQUISA
-pyautogui.moveTo(380, 345, 2)
-pyautogui.doubleClick()
-pyautogui.write("01062024")
-time.sleep(5)
-pyautogui.press("enter")
-time.sleep(30)
+def filtrar_data():
+    pyautogui.moveTo(380, 345, 2)
+    pyautogui.doubleClick()
+    pyautogui.write("01062024")
+    time.sleep(5)
+    pyautogui.press("enter")
+    time.sleep(30)
 
 # CASO HAJA ALGUMA PLANILHA NA PASTA, DELETA
-if os.path.exists("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeVermelha.xlsx"):
-    os.remove("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeVermelha.xlsx")
+def excluindo_planilhas_desatualizadas():
+    if os.path.exists("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeVermelha.xlsx"):
+        os.remove("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeVermelha.xlsx")
 
-if os.path.exists("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeAmarela.xlsx"):
-    os.remove("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeAmarela.xlsx")
-
-
-# ABRINDO PLANILHA
-pyautogui.moveTo(500, 885, 2)
-pyautogui.click()
-time.sleep(5)
-pyautogui.keyDown("alt")
-pyautogui.keyDown("shift")
-pyautogui.press("tab")
-pyautogui.keyUp("shift")
-pyautogui.keyUp("alt")
-time.sleep(2)
-
-# SALVAR PLANILHA
-pyautogui.keyDown("alt")
-pyautogui.press("F4")
-pyautogui.keyUp("alt")
-pyautogui.press("enter")
-pyautogui.write("planilha_sdes_vermelhas")
-pyautogui.moveTo(600, 60, 2)
-pyautogui.click()
-time.sleep(1)
-pyautogui.write("C:\\Users\\joaov\\Desktop\\PROGRAMA")
-pyautogui.press("enter")
-pyautogui.moveTo(750, 590, 2)
-pyautogui.click()
-
-# FILTRANDO AS SDES AMARELAS
-time.sleep(2)
-pyautogui.moveTo(1500, 345, 2)
-pyautogui.click()
-time.sleep(2)
-pyautogui.moveTo(1500, 430, 2)
-pyautogui.press("enter")
-time.sleep(5)
+    if os.path.exists("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeAmarela.xlsx"):
+        os.remove("C:\\Users\\joaov\\Desktop\\PROGRAMA\\sdeAmarela.xlsx")
 
 # ABRINDO PLANILHA
-pyautogui.moveTo(500, 885, 2)
-pyautogui.click()
-time.sleep(5)
-pyautogui.keyDown("alt")
-pyautogui.keyDown("shift")
-pyautogui.press("tab")
-pyautogui.keyUp("shift")
-pyautogui.keyUp("alt")
-time.sleep(2)
+def abrir_planilha():
+    pyautogui.moveTo(500, 885, 2)
+    pyautogui.click()
+    time.sleep(5)
+    pyautogui.keyDown("alt")
+    pyautogui.keyDown("shift")
+    pyautogui.press("tab")
+    pyautogui.keyUp("shift")
+    pyautogui.keyUp("alt")
+    time.sleep(2)
 
 # SALVAR PLANILHA
-pyautogui.keyDown("alt")
-pyautogui.press("F4")
-pyautogui.keyUp("alt")
-pyautogui.press("enter")
-pyautogui.write("planilha_sdes_amarelas")
-pyautogui.moveTo(600, 60, 2)
-pyautogui.click()
-time.sleep(1)
-pyautogui.write("C:\\Users\\joaov\\Desktop\\PROGRAMA")
-pyautogui.press("enter")
-pyautogui.moveTo(750, 590, 2)
-pyautogui.click()
+def salvar_planilha(nome_planilha):
+    pyautogui.keyDown("alt")
+    pyautogui.press("F4")
+    pyautogui.keyUp("alt")
+    pyautogui.press("enter")
+    pyautogui.write(nome_planilha)
+    pyautogui.moveTo(600, 60, 2)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.write("C:\\Users\\joaov\\Desktop\\PROGRAMA")
+    pyautogui.press("enter")
+    pyautogui.moveTo(750, 590, 2)
+    pyautogui.click()
+
 
 ##############################################################################
+
+usuario = "joaov"
+coordenadas_vermelhas = (1500, 405, 2)
+coordenadas_amarelas = (1500, 430, 2)
+
+filtrar_usuario(usuario)
+
+filtrar_sdes(coordenadas_vermelhas)
+filtrar_data()
+excluindo_planilhas_desatualizadas()
+abrir_planilha()
+salvar_planilha("planilha_sdes_vermelhas")
+
+filtrar_sdes(coordenadas_amarelas)
+abrir_planilha()
+salvar_planilha("planilha_sdes_amarelas")
